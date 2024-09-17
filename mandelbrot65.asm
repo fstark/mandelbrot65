a14008a0f93400841a0b57a6ef188b7987b0829f
Frederic Stark <frederic.stark@gmail.com>
2024-09-17 20:31:40 +0200

    Work in progress

--- mandelbrot65.asm
+++ mandelbrot65.asm
@@ -5,21 +5,20 @@ FACC = $00
 NUM = $02  ; 3 bytes
 INCR = $05 ; 3 bytes
 PTR = $08
+TMP = $0A
 
 ; Format of numbers (16 bits, stored little endian -- reversed from this drawing)
 ; +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+
-; |S|N| |1|i|i|i|f| |f|f|f|f|f|f|f|0|
+; |S| | |1|i|i|i|f| |f|f|f|f|f|f|f|0|
 ; +-+-+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+
 ; S = Sign
-; N = NaN
 ; 1 is for addressing 0x1000 for square table
 ; iii = integer part (3 bits)
 ; ffffffff = fractional part (8 bits)
 ; 0 is for having even addresses in the square table
-; Note we could reuse the addressing bit for NaN, as NaN cannot be squared
 
+SIGNBIT = $80
 NUMBIT = $10
-NANMASK = $40
 
 ECHO = $FFEF
 WOZMON = $FF00
@@ -27,7 +26,6 @@ WOZMON = $FF00
 SQUARETABLE = $1000			; This table cannot be moved
 SQUARETABLE_END = $2000		; End of table
 
-; Sets numbers to 0
 #define MZEROT(ADRS) LDA #0:STA ADRS:STA ADRS+1:STA ADRS+2
 
 ; Increments
@@ -108,11 +106,11 @@ FILLNAN:
 	RTS
 
 CONTINUE:
+		; 0,0 is not a number (missing the NUMBIT)
 	LDY #0
 	LDA #0
 	STA (PTR),Y
 	INY
-	LDA #NANMASK
 	STA (PTR),Y
 
 			; Increment pointer
@@ -129,4 +127,82 @@ SQUARE:
 	LDA (FACC),Y
 	TAX
 	LDA (FACC+1),Y
+	RTS
+
+
+; Load square of number in (A,X)
+; Does not manage NaNs
+; Z set if NaN
+SQN_AX:
+	LDY #0
+	LDA (NUM),Y
+	TAX
+	INY
+	LDA (NUM),Y
+	AND #NUMBIT
+	RTS
+
+	; Creates a zero in (AX)
+#define MZEROAX LDX #$0: LDA #NUMBIT
+
+	; Loads A number into AX
+#define MLOADAX(NUM) LDX NUM: LDA NUM+1
+
+	; Stores AX into NUM
+#define MSTOREAX(NUM) STX NUM: STA NUM+1
+
+	; Z if NUM is NaN
+#define MISNAN(NUM) LDA NUM+1: AND #NUMBIT
+
+	; Adds NUM with AX
+	; NUM & AX are of same sign
+	; Z if overflow
+ADD_AX_SS:
+	CLC
+	PHA
+	TXA
+	ADC NUM
+	TAX
+	PLA
+	ADC NUM+1
+	EOR #NUMBIT
+	RTZ
+	AND #$8F ; Clear unused bits (so next operation don't overflow into sign)
+	ORA #$10  ; Sets Number bit
+	RTS
+
+	; Push (A,X) into stack
+#define MPUSHAX STA TMP: TXA: PHA: LDA TMP: PHA
+
+	; Restore (A,X) from stack
+#define MPULLAX PLA: TAX: PLA
+
+	; Prints the number. No change to any register. Trashes TMP
+PRINT_AX:
+	PHP
+	MPUSHAX
+	PHA
+	BIT #SIGNBIT
+	BEQ POSITIVE
+	LDA #'+'
+	BEQ PRINTSIGN
+POSITIVE:
+	LDA #'-'
+PRINTSIGN:
+	JSR ECHO
+	PLA
+	PHA
+	ROR A
+	AND #$07
+	ADD #'0'
+	JSR ECHO
+	LDA #'.'
+	JSR ECHO
+	PLA
+	ROR A
+	TXA
+	ROR A
+	JSR PRHEX
+	MPULLAX
+	PLP
 	RTS
